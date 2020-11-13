b8ff132d9948708872e6d95a3e390ee7bd218a53
dingjingmaster <dingjing@live.cn>
2020-11-13 14:31:36 +0800

    修改镜像参数

--- profiledef.sh
+++ profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="gracefullinux"
+iso_label="GracefulLinux $(date +%Y%m)"
+iso_publisher="GracefulLinux <https://github.com/graceful-linux>"
+iso_application="GracefulLinux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
-install_dir="arch"
+install_dir="gracefullinux"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
