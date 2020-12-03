52a3d42e506ceaeff4fb1e09b1e3790ea1447312
dingjingmaster <dingjing@live.cn>
2020-12-03 18:57:45 +0800

    添加 gnome-shell 插件

--- airootfs/root/customize-gracefullinux.sh
+++ airootfs/root/customize-gracefullinux.sh
@@ -32,5 +32,29 @@ EOF
 systemctl set-default graphical.target
 systemctl enable gdm.service
 
+# 设置 dock
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-favorites true
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+gsettings set org.gnome.shell.extensions.dash-to-dock animate-show-apps true
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-windows-preview true
+gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner true
+
+gsettings set org.gnome.shell.extensions.dash-to-dock height-fraction 0.8
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.26
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock scroll-action 'cycle-windows'
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'focus-or-previews'
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'SQUARES'
+
+dconf write /org/gnome/shell/disabled-extensions = ['nightthemeswitcher@romainvigier.fr', 'desktop-icons@csoriano', 'openweather-extension@jenslody.de', 'gsconnect@andyholmes.github.io', 'drive-menu@gnome-shell-extensions.gcampax.github.com', 'launch-new-instance@gnome-shell-extensions.gcampax.github.com', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'dynamic-panel-transparency@rockon999.github.io', 'compiz-windows-effect@hermes83.github.com', 'panel-osd@berend.de.schouwer.gmail.com', 'gamemode@christian.kellner.me', 'appindicatorsupport@rgcjonas.gmail.com', 'native-window-placement@gnome-shell-extensions.gcampax.github.com', 'clipboard-indicator@tudmotu.com', 'ding@rastersoft.com', 'arch-update@RaphaelRochet', 'rss-feed@gnome-shell-extension.todevelopers.github.com', 'compiz-alike-windows-effect@hermes83.github.com', 'dash-to-dock@micxgx.gmail.com', 'auto-move-windows@gnome-shell-extensions.gcampax.github.com']
+
+
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
